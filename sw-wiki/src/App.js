b031dfe62b4442c2589686cb0656fab698a57bb6
German <german.ampuero@unmsm.edu.pe>
2022-03-29 23:17:59 -0500

    This is where the fun beginsssssss

--- sw-wiki/src/App.js
+++ sw-wiki/src/App.js
@@ -3,17 +3,18 @@ import CharactersPage from "./components/Pages/CharactersPage";
 import PlanetsPage from "./components/Pages/PlanetsPage";
 import StarshipsPage from "./components/Pages/StarshipsPage";
 
-function App(){
+function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element = {<CharactersPage/>}/>
-        <Route path="/planets"element = {<PlanetsPage/>} />
-        <Route path="/starships"element = {<StarshipsPage/>} />
-       </Routes>
+        <Route path="/" element={<CharactersPage />}>
+          <Route index element={<CharactersPage />} />
+        </Route>
+        <Route path="starships" element={<StarshipsPage />} />
+        <Route path="planets" element={<PlanetsPage />} />
+      </Routes>
     </BrowserRouter>
-  )
-
+  );
 }
 
 export default App;
